f4407a1bac9f5821074a898b3bc5f9e0aabb0fe1
Ric (Ryszard) Szopa <ryszard.szopa@gmail.com>
2016-03-02 18:51:43 +0100

    Allow to set flags from the environment.
    A flag named "foo_bar" will be set from the environment variable
    DOORMAN_FOO_BAR.

---
+++ go/flagenv/flagenv.go
@@ -0,0 +1,69 @@
+// Package flagenv allows setting flags from the command line.
+package flagenv
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/golang/glog"
+)
+
+// NOTE(ryszard): This code is heavily inspired by
+// https://github.com/coreos/etcd/blob/master/pkg/flags/flag.go.
+
+// Populate sets the flags in set from the environment. The
+// environment value used will be the name of the flag in upper case,
+// with '-' changed to '_', and (if prefixis not the empty string)
+// prepended with prefix and an underscore. So, if prefix is
+// "DOORMAN", and the flag's name "foo-bar", the environment variable
+// DOORMAN_FOO_BAR will be used.
+func Populate(set *flag.FlagSet, prefix string) error {
+	var (
+		setThroughFlags = make(map[string]bool)
+		knownEnv        = make(map[string]bool)
+		err             error
+	)
+	set.Visit(func(f *flag.Flag) {
+		setThroughFlags[f.Name] = true
+	})
+
+	set.VisitAll(func(f *flag.Flag) {
+		key := flagToEnv(prefix, f.Name)
+		knownEnv[key] = true
+		val := os.Getenv(key)
+		if val == "" {
+			return
+		}
+
+		if setThroughFlags[f.Name] {
+			log.Warningf("Recognized environment variable %v, but shadowed by flag %v: won't be used.", key, f.Name)
+			return
+		}
+		if e := set.Set(f.Name, val); e != nil {
+			err = fmt.Errorf("Invalid value %q for %v.", val, key)
+			return
+		}
+	})
+
+	for _, env := range os.Environ() {
+		kv := strings.SplitN(env, "=", 2)
+		if len(kv) < 1 {
+			continue
+		}
+		if name := kv[0]; strings.HasPrefix(name, prefix) && !knownEnv[env] {
+			log.Warningf("Unrecognized environment variable %s", name)
+		}
+	}
+
+	return err
+}
+
+func flagToEnv(prefix, name string) string {
+	rest := strings.ToUpper(strings.Replace(name, "-", "_", -1))
+	if prefix != "" {
+		return prefix + "_" + rest
+	}
+	return rest
+}
---
+++ go/flagenv/flagenv_test.go
@@ -0,0 +1,36 @@
+package flagenv
+
+import (
+	"flag"
+	"os"
+	"testing"
+)
+
+func TestPopulate(t *testing.T) {
+
+	var (
+		fs  = flag.NewFlagSet("testing", flag.ExitOnError)
+		foo = fs.String("foo", "", "")
+		bar = fs.String("bar", "", "")
+		baz = fs.String("baz", "", "")
+	)
+	fs.Parse([]string{"-foo=foo", "-baz=baz"})
+
+	os.Clearenv()
+	os.Setenv("DOORMAN_BAR", "bar")
+	os.Setenv("DOORMAN_BAZ", "baz from env")
+
+	if err := Populate(fs, "DOORMAN"); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := *foo, "foo"; got != want {
+		t.Errorf("foo=%q; want %q", got, want)
+	}
+	if got, want := *bar, "bar"; got != want {
+		t.Errorf("bar=%q; want %q", got, want)
+	}
+	if got, want := *baz, "baz"; got != want {
+		t.Errorf("baz=%q; want %q", got, want)
+	}
+}
